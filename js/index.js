42435f5cf36e9cfb86e03f34c0d51a6e839ff618
juego <150973333+aaron3110@users.noreply.github.com>
2024-08-07 20:05:21 -0600

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,39 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const elementsToAnimate = document.querySelectorAll('section, img, h1, h2, h3, p, div, .overlay, .header-text, header');
+
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('animated');
+                if (entry.target.tagName === 'IMG' || entry.target.tagName === 'DIV' || entry.target.tagName === 'SECTION' || entry.target.classList.contains('overlay') || entry.target.classList.contains('header-text') || entry.target.tagName === 'HEADER') {
+                    entry.target.classList.add('fade-in');
+                } else {
+                    entry.target.classList.add('slide-up');
+                }
+                if (entry.target.tagName === 'HEADER') {
+                    entry.target.classList.add('slide-down');
+                }
+            } else {
+                entry.target.classList.remove('animated', 'fade-in', 'slide-up', 'slide-down');
+            }
+        });
+    });
+
+    elementsToAnimate.forEach(element => {
+        observer.observe(element);
+    });
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    const groups = document.querySelectorAll('.content-box > .group');
+    let currentIndex = 0;
+
+    function showNextGroup() {
+        groups.forEach(group => group.classList.remove('active'));
+        groups[currentIndex].classList.add('active');
+        currentIndex = (currentIndex + 1) % groups.length;
+    }
+
+    showNextGroup();
+    setInterval(showNextGroup, 7000); // Cambia cada 5 segundos
+});
